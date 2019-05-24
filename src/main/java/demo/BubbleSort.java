30df83894ad878360a085f5e3e62e0cb3ddec991
liuxin1 <204060781@qq.com>
2019-05-24 11:20:47 +0800

    1

---
+++ src/main/java/demo/BubbleSort.java
@@ -0,0 +1,24 @@
+package demo;
+
+public class BubbleSort {
+    public static void main(String[] args){
+        String s="aaahssjhssssssssshwws123222";
+        // 转化为字符数组。
+        char[] c=s.toCharArray();
+        int[] b=new int[127];
+        for(int i=0;i<c.length-1;i++){
+            b[c[i]]++;
+        }
+        int max=b[0];   //定义次数
+        int value=0;    //定义最大值
+        for(int i=1;i<b.length;i++){
+            if(b[i]>max){
+                max=b[i];
+                value=i;
+            }
+        }
+        System.out.print("出现次数最多的值是:"+(char)value+", 出现了"+max+"次");
+    }
+
+}
+
