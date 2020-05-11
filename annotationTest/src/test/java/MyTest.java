ab5f64b9ff34c488f53a3cdff00d277d99b29008
liuxin@lihfinance.com <liuxin2019>
2020-05-11 14:48:48 +0800

    1

--- annotationTest/src/test/java/MyTest.java
+++ annotationTest/src/test/java/MyTest.java
@@ -8,13 +8,16 @@ import java.util.List;
  * @create: 2019-01-16 15:32
  **/
 public class MyTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Person person = new Person();
- //       person.setName("张飞");
-        person.setAge("128w");
+        //person.setName("张飞");
+        person.setAge("128");
         StringBuilder validate = MyAnnotation.validate(person);
 
-        System.err.println(validate.toString());
+        if (validate.toString().contains(",")){
+            throw new Exception(validate.toString()+"123");
+        }
+        System.out.println("12345");
     }
 }
 
